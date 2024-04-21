c413cd963fc450d1dc9b4cb793ceccbeb88944ef
Ilham Nur Hermawan <127424043+0xilham@users.noreply.github.com>
2024-04-21 17:15:59 +0700

    Add Interact NPC Dialogue
    Interact NPC with Dialogue

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Movement");
     }
     public void QuitGame()
     {
